bc5293d4c77b686dfd62d42da6b2f30cda8174ec
DiegoOrlando17 <diegoorlando.1997@gmail.com>
2023-07-04 11:28:39 -0300

    First Commit

---
+++ JavierApp/JavierApp/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace JavierApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFiltros = new System.Windows.Forms.TextBox();
+            this.btFiltrar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTipoFiltro = new System.Windows.Forms.ComboBox();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.rtbFiltros = new System.Windows.Forms.RichTextBox();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seleccione";
+            // 
+            // tbFiltros
+            // 
+            this.tbFiltros.Location = new System.Drawing.Point(270, 26);
+            this.tbFiltros.Name = "tbFiltros";
+            this.tbFiltros.Size = new System.Drawing.Size(514, 22);
+            this.tbFiltros.TabIndex = 4;
+            // 
+            // btFiltrar
+            // 
+            this.btFiltrar.Location = new System.Drawing.Point(790, 60);
+            this.btFiltrar.Name = "btFiltrar";
+            this.btFiltrar.Size = new System.Drawing.Size(109, 27);
+            this.btFiltrar.TabIndex = 7;
+            this.btFiltrar.Text = "Enviar";
+            this.btFiltrar.UseVisualStyleBackColor = true;
+            this.btFiltrar.Click += new System.EventHandler(this.btFiltrar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valores";
+            // 
+            // cbTipoFiltro
+            // 
+            this.cbTipoFiltro.FormattingEnabled = true;
+            this.cbTipoFiltro.Items.AddRange(new object[] {
+            "Usuarios",
+            "Perfiles",
+            "Transacciones",
+            "Grupos"});
+            this.cbTipoFiltro.Location = new System.Drawing.Point(96, 25);
+            this.cbTipoFiltro.Name = "cbTipoFiltro";
+            this.cbTipoFiltro.Size = new System.Drawing.Size(168, 24);
+            this.cbTipoFiltro.TabIndex = 10;
+            this.cbTipoFiltro.SelectedIndexChanged += new System.EventHandler(this.cbTipoFiltro_SelectedIndexChanged);
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(790, 25);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(109, 27);
+            this.btAgregar.TabIndex = 11;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // rtbFiltros
+            // 
+            this.rtbFiltros.Location = new System.Drawing.Point(20, 156);
+            this.rtbFiltros.Name = "rtbFiltros";
+            this.rtbFiltros.Size = new System.Drawing.Size(764, 268);
+            this.rtbFiltros.TabIndex = 12;
+            this.rtbFiltros.Text = "";
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(790, 93);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(109, 27);
+            this.btCancelar.TabIndex = 13;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // btSalir
+            // 
+            this.btSalir.Location = new System.Drawing.Point(790, 434);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(108, 27);
+            this.btSalir.TabIndex = 14;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 479);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.rtbFiltros);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.cbTipoFiltro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btFiltrar);
+            this.Controls.Add(this.tbFiltros);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Consulta Tabla Expandida";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFiltros;
+        private System.Windows.Forms.Button btFiltrar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbTipoFiltro;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.RichTextBox rtbFiltros;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btSalir;
+    }
+}
+
---
+++ JavierApp/JavierApp/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JavierApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DeleteAllTxts();
+            cbTipoFiltro.SelectedItem = "Usuarios";
+        }
+        
+        private void DeleteAllTxts()
+        {
+            //foreach (string sFile in Directory.GetFiles(ConfigurationManager.AppSettings["Directory"], "*.txt"))
+            //{                
+            //    File.Delete(sFile);
+            //}
+            File.Delete(ConfigurationManager.AppSettings["OutputTXT"]);
+            File.Delete(ConfigurationManager.AppSettings["ErrorTXT"]);
+        }
+
+        List<string> filtros = new List<string>();
+        string tipo;
+        private void btAgregar_Click(object sender, EventArgs e)
+        {
+            if (cbTipoFiltro.SelectedItem == null || string.IsNullOrWhiteSpace(tbFiltros.Text))
+                return;
+            if (string.IsNullOrWhiteSpace(tipo))
+            {
+                tipo = cbTipoFiltro.SelectedItem.ToString();
+                cbTipoFiltro.Enabled = false;
+            }
+
+            string filtro;
+            switch (tipo)
+            {
+                case "Usuarios":
+                case "Grupos":
+                    filtro = tbFiltros.Text.Substring(0, tbFiltros.Text.Length > 8 ? 8 : tbFiltros.Text.Length);
+                    break;
+                case "Transacciones":
+                    filtro = tbFiltros.Text.Substring(0, tbFiltros.Text.Length > 4 ? 4 : tbFiltros.Text.Length);
+                    break;
+                case "Perfiles":
+                    filtro = tbFiltros.Text.Substring(0, tbFiltros.Text.Length > 44 ? 44 : tbFiltros.Text.Length);
+                    break;
+                default:
+                    filtro = "";
+                    break;
+            }
+            filtros.Add(filtro);
+            rtbFiltros.Text += filtro + Environment.NewLine;
+
+            tbFiltros.Clear();
+            tbFiltros.Focus();
+        }
+
+        private void btFiltrar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(tipo))
+            {
+                string header = "**" + tipo + "--" + System.Security.Principal.WindowsIdentity.GetCurrent().Name + "\\(" + GetIPAddress() + ")";
+                WriteTxt(ConfigurationManager.AppSettings["FiltrosTXT"], header, filtros);
+
+                ExecuteBAT();
+
+                ClearAll();
+            }
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ClearAll()
+        {
+            filtros.Clear();
+            rtbFiltros.Text = "";
+            tbFiltros.Clear();
+            tipo = string.Empty;
+            cbTipoFiltro.Enabled = true;
+        }
+
+        private void WriteTxt(string txtPath, string header, List<string> values)
+        {
+            if (!File.Exists(txtPath))
+            {
+                File.Create(txtPath).Close();
+            }
+            TextWriter clean = new StreamWriter(txtPath);
+            clean.Write("");
+            clean.Close();
+
+            TextWriter tw = new StreamWriter(txtPath, true);
+            if (!string.IsNullOrWhiteSpace(header))
+                tw.WriteLine(header);
+
+            foreach (var value in values)
+            {
+                tw.WriteLine(value);
+            }
+            tw.Close();
+        }
+
+        private void WriteTxt(string txtPath, string header, string value)
+        {
+            if (!File.Exists(txtPath))
+            {
+                File.Create(txtPath).Close();
+            }
+            TextWriter clean = new StreamWriter(txtPath);
+            clean.Write("");
+            clean.Close();
+
+            TextWriter tw = new StreamWriter(txtPath, true);
+            if (!string.IsNullOrWhiteSpace(header))
+                tw.WriteLine(header);
+
+            tw.WriteLine(value);
+            tw.Close();
+        }
+
+        private string GetIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void ExecuteBAT()
+        {
+            var processInfo = new ProcessStartInfo(ConfigurationManager.AppSettings["FTPBAT"]);
+
+            processInfo.CreateNoWindow = true;
+
+            processInfo.UseShellExecute = false;
+
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.Start();
+
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            WriteTxt(ConfigurationManager.AppSettings["OutputTXT"], "", output);
+            string error = process.StandardError.ReadToEnd();
+            WriteTxt(ConfigurationManager.AppSettings["ErrorTXT"], "", error);
+        }
+
+        private void cbTipoFiltro_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbFiltros.Focus();
+        }
+    }
+}
